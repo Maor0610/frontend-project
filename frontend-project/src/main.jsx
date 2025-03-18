6324c6cfa6ac79176e38e422789de0413b556128
Maor <maoredri2@gmail.com>
2025-03-18 22:14:02 +0200

    install metirial ui

--- frontend-project/src/main.jsx
+++ frontend-project/src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from "react-router-dom";
+import Header from './Header.jsx';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Header />
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
